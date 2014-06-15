f3e66a48d49369d018e7e7fb70be22da8682b085
Hiroki Tanaka <dabits.net@gmail.com>
2014-06-15 23:43:08 +0900

    use call_user_func_array.

--- model/Storage.php
+++ model/Storage.php
@@ -3,7 +3,9 @@ require_once APP_ROOT.'/model/Config.php';
 require_once __DIR__.'/S3.php';
 require_once __DIR__.'/HDD.php';
 
-class Storage {
+class Storage
+{
+	private static $class_name;
 
 	protected function __construct()
 	{
@@ -11,31 +13,34 @@ class Storage {
 
 	public static function uploadData($key,$data,$type,$acl='private')
 	{
-		$m = Config::get('storage');
-		return $m::uploadData($key,$data,$type,$acl);
+		return call_user_func_array(array(self::getClassName(), __FUNCTION__), func_get_args());
 	}
 
 	public static function uploadFile($key,$filename,$type,$acl='private')
 	{
-		$m = Config::get('storage');
-		return $m::uploadFile($key,$filename,$type,$acl);
+		return call_user_func_array(array(self::getClassName(), __FUNCTION__), func_get_args());
 	}
 
 	public static function rename($srckey,$dstkey,$acl='private')
 	{
-		$m = Config::get('storage');
-		return $m::rename($srckey,$dstkey,$acl);
+		return call_user_func_array(array(self::getClassName(), __FUNCTION__), func_get_args());
 	}
 
 	public static function delete($key)
 	{
-		$m = Config::get('storage');
-		return $m::delete($key);
+		return call_user_func_array(array(self::getClassName(), __FUNCTION__), func_get_args());
 	}
 
 	public static function url($key,$expires=null)
 	{
-		$m = Config::get('storage');
-		return $m::url($key,$expires);
+		return call_user_func_array(array(self::getClassName(), __FUNCTION__), func_get_args());
+	}
+	
+	private static function getClassName()
+	{
+		if(! ( self::$class_name || (self::$class_name = Config::get('storage')) ) ){
+			throw new Exception('`storage` config is needed.');
+		}
+		return self::$class_name;
 	}
 }
